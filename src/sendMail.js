a832b023bef9507e8cfab3c379d17791cf5666b4
suary <604791489@qq.com>
2017-12-07 17:56:29 +0800

    move receiver
    move receiver & add choose

--- src/sendMail.js
+++ src/sendMail.js
@@ -1,6 +1,6 @@
 const nodemailer = require("nodemailer");
 
-exports.send = (html, attachments) => {
+exports.send = (html, attachments,receiver) => {
   //配置邮件
   let transporter = nodemailer.createTransport({
     host: "smtp.163.com",
@@ -11,22 +11,10 @@ exports.send = (html, attachments) => {
       pass: "eslintno1" 
     }
   });
-  let toList = [
-    //"15412@etransfar.com",
-    // "15621@etransfar.com",
-    // "16639@etransfar.com",
-    // "12613@etransfar.com",
-     "15617@etransfar.com",
-    // "17323@etransfar.com",
-    // "17356@etransfar.com",
-    // "15145@etransfar.com",
-    // "12504@etransfar.com",
-    // "17196@etransfar.com",
-    // "geek_he@126.com"
-  ];
+  
   let option = {
     from: '"ehdfe_public"<ehdfe_public@163.com>',
-    to: toList.join(",")/* ,
+    to: receiver.join(",")/* ,
     bcc: "doublejoke@163.com" */
   };
   option.subject = "EHDFE Eslint Report";
